7e15ed16abc47d08750419e8e4007f8d46c772e5
Diocleniax789 <julioramdom123@gmail.com>
2024-12-24 01:55:44 -0300

    se han agregado los prototipos

--- gestor-contras.c
+++ gestor-contras.c
@@ -5,6 +5,11 @@ struct datos{
     char clave[4][9];
 };
 
+struct datos * carga(int*);
+struct datos * valida(char*,char*,struct datos*,int);
+char * verifica (struct datos);
+void reemplaza (struct datos *, char *);
+
 int main(){
 
     return 0;
