455ec9f7c545cfb2eaf26a356bf58d8302e3e081
Diocleniax789 <julioramdom123@gmail.com>
2024-12-24 22:20:07 -0300

    se han completado el primer prototipo de carga de todos los usuarios

--- gestor-contras.c
+++ gestor-contras.c
@@ -22,7 +22,7 @@ int main(){
 
     todos_los_usuarios = carga(&usuarios_cargados);
 
-    do {
+   /* do {
 
         fflush(stdin);
         printf("\n + Ingrese nombre usuario: ");
@@ -43,12 +43,53 @@ int main(){
 
         } while(op != 's' && op != 'n');
 
-    } while(op != 'n');
+    } while(op != 'n');*/
 
     return 0;
 }
 
 struct datos * carga(int *usuarios_cargados){
+   int i,j;
+   char nom_usu[20] = {0}, contrasenia[9] = {0};
+   static struct datos todos_los_usuarios[1000];
+
+   for(i = 0; i < 1000; i++) {
+        system("cls");
+        fflush(stdin);
+        printf("\n /// PARA FINALIZAR CON LA CARGA, SIMPLEMENTE AL COLOCAR EL NOMBRE DE USUARIO, ESCRIBA '*' /// \n");
+        printf("\n");
+        printf("\n + Nombre de usuario: ");
+        scanf("%s",nom_usu);
+        if(strcmp(nom_usu,"*") == 0) {
+            break;
+        } else {
+            strcpy(todos_los_usuarios[i].nom_usuario,nom_usu);
+            printf("\n * USUARIO CARGADO CON EXITO * \n");
+            for(j = 0; j < 4; j++){
+                do {
+                    fflush(stdin);
+                    printf("\n Contrasenia nro %i",j + 1);
+                    printf("\n + Ingrese la contrasenia: ");
+                    scanf("%s",contrasenia);
+                    if(strlen(contrasenia) > 9) {
+                        printf("\n");
+                        printf("\n xxxx La contrasenia no debe superar los 9 caracteres xxxx \n");
+                    }
+                } while(strlen(contrasenia) > 9);
+
+                strcpy(todos_los_usuarios[i].clave[j],contrasenia);
+                printf("\n * CONTRASENIA CARGADA CON EXITO * \n");
+            }
+
+            (*usuarios_cargados)++;
+        }
+
+        printf("\n");
+        system("pause");
+   }
 
 
+   printf("\n *** Todos los usuarios han sido cargados con exito! *** \n");
+
+   return todos_los_usuarios;
 }
